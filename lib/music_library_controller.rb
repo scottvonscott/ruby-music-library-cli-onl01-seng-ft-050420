1c530e0bd2bb30a270b27b520c14f994420631c6
Scott  Keplinger <skeplin17@gmail.com>
2020-05-24 19:06:26 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -92,7 +92,7 @@ class MusicLibraryController
     sorted_songs = Song.all.sort_by {|song| song.name}
     number_choice = gets.chomp.to_i
     if (1..Song.all.size).include?(number_choice)
-      puts "Playing #{sorted_songs[number_choice].song.name} by #{sorted_songs[number_choice].artist.name}"
+      puts "Playing #{sorted_songs[number_choice].name} by #{sorted_songs[number_choice].artist.name}"
     else
       gets.chomp
     end
