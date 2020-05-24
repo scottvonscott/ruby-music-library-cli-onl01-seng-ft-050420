1db7cefb81f83e179d5c3b6c49692d30499dbaf7
Scott  Keplinger <skeplin17@gmail.com>
2020-05-24 17:53:41 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -56,7 +56,6 @@ class MusicLibraryController
     puts "Please enter the name of an artist:"
     artist_choice = gets.chomp
     sorted_artist = []
-
       Artist.all.detect do |artist|
         if artist.name == artist_choice
             sorted_artist = artist.songs.sort_by do |song|
